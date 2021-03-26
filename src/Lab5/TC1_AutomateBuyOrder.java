58f7dca71ac16ed456d5ef867cd3137cafab6908
HiamKaito <tangchichung1872014@gmail.com>
2021-03-26 14:22:39 +0700

    Lab 6

---
+++ src/Lab5/TC1_AutomateBuyOrder.java
@@ -0,0 +1,109 @@
+package Lab5;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class TC1_AutomateBuyOrder {
+	public static void main(String[] args) {
+		// file path
+		String ChromeDriver = new File("").getAbsolutePath() +"\\chromedriver.exe";
+		// add driver
+		System.setProperty("Webdriver.chrome.driver",ChromeDriver);
+		WebDriver driver = new ChromeDriver();
+		// add web url
+		driver.get("http://automationpractice.com/index.php");
+		
+		//Resize current window to the set dimension
+	    driver.manage().window().maximize();
+	    
+		// step 2
+		// click login button
+		driver.findElement(By.className("login")).click();
+		
+		// fill email
+		driver.findElement(By.id("email")).sendKeys("email.com@gmail.com");
+		driver.findElement(By.id("passwd")).sendKeys("passworrdddddd");
+		
+		// login
+		driver.findElement(By.id("SubmitLogin")).click();
+		
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// step 3
+		// move mouse to women
+		Actions mouse = new Actions(driver);
+		mouse.moveToElement(driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[1]/a"))).perform();
+		
+		// step 4
+		// click t-shirt
+		driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[1]/ul/li[1]/ul/li[1]/a")).click();
+		
+		// step 5, 6
+		// hover product and click it
+		mouse.moveToElement(driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li"))).perform();
+		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li/div/div[2]/div[2]/a[2]")).click();
+		
+		// step 7
+		// inc number
+		driver.findElement(By.xpath("//*[@id=\"quantity_wanted_p\"]/a[2]")).click();
+		
+		// step 8, 9
+		// select size L and color
+		Select size = new Select(driver.findElement(By.id("group_1")));
+		size.selectByIndex(2);
+		
+		driver.findElement(By.id("color_14")).click();
+		
+		// step 10
+		// click add
+		driver.findElement(By.name("Submit")).click();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// step 11
+		// select process
+		driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a")).click();
+		
+		// process to checkout
+		driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+
+		// process address
+		driver.findElement(By.name("processAddress")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+
+		// shipping
+		driver.findElement(By.id("cgv")).click();
+		driver.findElement(By.name("processCarrier")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+
+		// payment
+		driver.findElement(By.xpath("//*[@id=\"HOOK_PAYMENT\"]/div[2]/div/p/a")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+		
+		// confim payment
+		driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button")).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
+		
+		// is good?
+		try {
+			String s = driver.findElement(By.xpath("//*[@id=\"center_column\"]/h1")).getText();
+			if (s.contains("ORDER CONFIRMATION")) 
+				System.out.println("Product is ordered");
+			else 
+				System.out.println("Fail");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("Finish");
+		// close
+		driver.close();
+	}
+	
+}
